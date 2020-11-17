318accf7f8960b5fbce6287da6056d84cd2c66b9
fnascimento <felipe.basina@gmail.com>
2020-11-16 21:02:55 -0300

    Adicionado exemplo de infix com objeto classe.

--- basics/InfixSample.kt
+++ basics/InfixSample.kt
@@ -3,6 +3,25 @@ fun main() {
     // Used to add operations to base 'Int' class (object)
     infix fun Int.times(str: String) = str.repeat(this);
     println(2 times "Bye");
-    println(2.times("Tschüss");
+    println(2.times("Tschüss"));
 
+
+    val sophia = Person("Sophia");
+    val claudia = Person("Claudia");
+
+    sophia likes claudia;
+
+    println("Sophia's liked people list is null or empty? " + sophia.likedPeople.isNullOrEmpty());
+    println("Claudia's liked people list is null or empty? " + claudia.likedPeople.isNullOrEmpty());
+
+    println("Sophia's liked people list:");
+    for (person in sophia.likedPeople) {
+        println(person.name);
+    }
+
+}
+
+class Person(val name: String) {
+    val likedPeople = mutableListOf<Person>();
+    infix fun likes(other: Person) { likedPeople.add(other); }
 }
