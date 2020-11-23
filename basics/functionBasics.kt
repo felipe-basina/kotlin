eac1ada4a425f8941cc7a15a27d77e3a26645553
fnascimento <felipe.basina@gmail.com>
2020-11-23 20:57:09 -0300

    Adicionado exemplos utilizando varargs.

--- basics/functionBasics.kt
+++ basics/functionBasics.kt
@@ -12,6 +12,19 @@ fun sumInt(x: Int, y: Int) : Int {
 
 fun sumIntInferred(x: Int, y: Int) = x + y;
 
+fun printVarargs(vararg messages: String) {
+    println(messages); // Prints memory address
+    for (message in messages) {
+        println(message);
+    }
+}
+
+fun printVarargsWithPrefix(vararg messages: String, prefix: String = "INFO") {
+    for (message in messages) {
+        println("${prefix} $message");
+    }
+}
+
 fun main() {
     printMessage("First message");
     printMessageWithPrefix("Another message without prefix");
@@ -19,4 +32,6 @@ fun main() {
     printMessageWithPrefix(prefix = "WARN", message = "A message....");
     println(sumInt(10, 6));
     println(sumIntInferred(10, 6));
+    printVarargs("Hallo", "Olá", "Hello", "Olla", "Kudos");
+    printVarargsWithPrefix("Hallo", "Olá", "Hello", "Olla", "Kudos", prefix = "WARN");
 }
