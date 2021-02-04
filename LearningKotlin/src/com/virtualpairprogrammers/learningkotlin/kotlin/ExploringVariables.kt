87a50db825a26969807d1ff3ad6be95965b54404
felipe-basina <felipe.basina@gmail.com>
2021-02-04 20:23:25 -0300

    Adicionado exemplos com string.

--- LearningKotlin/src/com/virtualpairprogrammers/learningkotlin/kotlin/ExploringVariables.kt
+++ LearningKotlin/src/com/virtualpairprogrammers/learningkotlin/kotlin/ExploringVariables.kt
@@ -1,9 +1,22 @@
 package com.virtualpairprogrammers.learningkotlin.kotlin
 
 fun main() {
-    var name: String = "Test"
-    val surname: String = "Another" // Immutable variable
+//    var name: String = "Test"
+//    val surname: String = "Another" // Immutable variable
+//    name = "Changed..."
+//    System.out.println(name + " " + surname)
 
-    name = "Changed..."
-    System.out.println(name + " " + surname)
+    var name = "Test"
+    val surname= "Another"
+    println("Hello $name ${surname.toUpperCase()}") // String templates
+    println("Your first name has ${name.length} characters")
+
+    val story = """It was a dark and stormy night
+        |A foul smell crept across the city
+        |Jane wondered what time it was, and when it would be daylight.""".trimMargin("|")
+    println(story)
+
+    val changedStory = story.replaceAfterLast("it", " would be dawn.")
+    println(changedStory)
+    println(changedStory.reversed())
 }
