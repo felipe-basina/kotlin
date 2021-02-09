7bfc85e86ef94426950ccdccbcfaba1e58984551
felipe-basina <felipe.basina@gmail.com>
2021-02-08 21:25:10 -0300

    Utilizacao de let + switch case.

--- LearningKotlinEx01/src/KotlinPerson.kt
+++ LearningKotlinEx01/src/KotlinPerson.kt
@@ -8,6 +8,35 @@ data class KotlinPerson(
     val dateOfBirth: Calendar?
 ) {
 
+    var favoriteColor: String? = null
+
+    fun getLastLetter(string: String) = string.takeLast(1)
+
+    fun getLastLetterOfColor(): String {
+        // return if (favoriteColor === null) "" else favoriteColor.takeLast(1) THIS WILL NOT WORK
+        // return favoriteColor?.let { this.getLastLetter(it) } ?: "" THIS WILL WORK ('it' when there is only one parameter)
+        return favoriteColor?.let { param -> this.getLastLetter(param) } ?: "" // THIS WILL WORK
+    }
+
+    fun getColorType(): String {
+        val color = getUpperCaseColor()
+//        return if (color == "")
+//            "empty"
+//        else if (color == "RED" || color == "BLUE" || color == "GREEN")
+//            "rgb"
+//        else
+//            "other"
+        return when (color) {
+            "" -> "empty"
+            "RED", "GREEN", "BLUE" ->  "rgb"
+            else -> "other"
+        }
+    }
+
+    fun getUpperCaseColor(): String {
+        return favoriteColor?.toUpperCase() ?: ""
+    }
+
     override fun toString() =
         "$title $firstName $surname"
 
@@ -34,6 +63,7 @@ data class KotlinPerson(
 
     // If age is not null then return age otherwise -1
     // This similar to if (age != null) age else -1
+    // Using the elvis operator
     val safeAge: Int
         get() = age ?: -1
 
