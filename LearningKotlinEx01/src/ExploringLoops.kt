7712e69531edfdc833a8790352e257a12281437e
felipe-basina <felipe.basina@gmail.com>
2021-02-08 21:48:19 -0300

    Adicionado exemplos para iteracao com foreach.

--- LearningKotlinEx01/src/ExploringLoops.kt
+++ LearningKotlinEx01/src/ExploringLoops.kt
@@ -28,4 +28,15 @@ fun main() {
     for ((key, value) in peopleMap) {
         println("$value has key $key")
     }
+
+    for (i in (0..9)) {
+        println(i)
+    }
+
+    (0..9).forEach{ index -> println(index) }
+    (0..9).forEach{ println(it) }
+    (9 downTo 0).forEach{ println(it) }
+    (0 until 9).forEach{ println(it) }
+    (0..9 step 2).forEach{ println(it) }
+    ('A'..'F').forEach{ println(it) }
 }
