0848886c197093b845bc11debe46ee6d97893535
felipe-basina <felipe.basina@gmail.com>
2021-02-15 10:45:00 -0300

    Adicionado exemplo com bloco try/catch.

---
+++ LearningKotlinEx01/src/ExploringExceptions.kt
@@ -0,0 +1,10 @@
+import java.lang.ArithmeticException
+
+// Exceptions in Kotlin are unchecked
+fun main() {
+    try {
+        println(7 / 0)
+    } catch (ae: ArithmeticException) {
+        println("caught")
+    }
+}
