8b4cbe30a265279877b81758f7f3333f3ee18223
felipe-basina <felipe.basina@gmail.com>
2021-02-15 08:47:37 -0300

    Adicionado exemplos de colecoes: set, lista imutavel, mapa.

--- LearningKotlinEx01/src/ExploringCollections.kt
+++ LearningKotlinEx01/src/ExploringCollections.kt
@@ -4,4 +4,20 @@ fun main() {
 
     val filteredColors = colors.filter { color -> color.toLowerCase().contains("r") }
     println(filteredColors)
+
+    val days = mutableListOf("Monday", "Tuesday", "Wednesday")
+    println(days)
+
+    val months = mutableSetOf("Jan", "Feb")
+    println(months)
+    val moreMonths = months.add("Mar")
+    println("months: $months, moreMonths? $moreMonths")
+
+    var notMutableMonthsSet = setOf("Jun", "Jul")
+    notMutableMonthsSet = notMutableMonthsSet.plus("Ago")
+    println(notMutableMonthsSet)
+
+    val webColors = mapOf("red" to "ff0000", "blue" to "00ff00")
+    println(webColors)
+    println(webColors["blue"])
 }
