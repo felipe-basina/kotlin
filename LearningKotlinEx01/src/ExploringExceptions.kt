984a1edf2486450fc06b380128a28378cc594783
felipe-basina <felipe.basina@gmail.com>
2021-02-15 10:52:50 -0300

    Adicionado exemplo para indicar a excecao que um metodo pode lancar.

--- LearningKotlinEx01/src/ExploringExceptions.kt
+++ LearningKotlinEx01/src/ExploringExceptions.kt
@@ -1,4 +1,14 @@
 import java.lang.ArithmeticException
+import kotlin.jvm.Throws
+
+// This annotation is just a hint to show the method my throw the specific exception
+// But when this method being called by Java code the compiler will complain forcing the Java code to
+// catch or throw it again
+@Throws(InterruptedException::class)
+fun divide(a: Int, b: Int): Double {
+    Thread.sleep(1000)
+    return a as Double / b
+}
 
 // Exceptions in Kotlin are unchecked
 fun main() {
