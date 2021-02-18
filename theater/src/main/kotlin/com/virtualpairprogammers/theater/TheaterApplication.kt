dde341429f5198ed444447a32c2cfc486fa125f3
felipe-basina <felipe.basina@gmail.com>
2021-02-18 20:55:46 -0300

    Adicionado dependencias para hibernate + base de dados em memoria + configuracoes para jpa.

--- theater/src/main/kotlin/com/virtualpairprogammers/theater/TheaterApplication.kt
+++ theater/src/main/kotlin/com/virtualpairprogammers/theater/TheaterApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TheaterApplication
+open class TheaterApplication
 
 fun main(args: Array<String>) {
 	runApplication<TheaterApplication>(*args)
