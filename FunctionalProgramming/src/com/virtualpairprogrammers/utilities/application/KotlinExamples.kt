cfe7e4c249c8562b2084bdd714f4cc8010d1c27f
felipe-basina <felipe.basina@gmail.com>
2021-02-18 19:09:08 -0300

    Adicionado exemplos programacao funcional com kotlin.

---
+++ FunctionalProgramming/src/com/virtualpairprogrammers/utilities/application/KotlinExamples.kt
@@ -0,0 +1,17 @@
+package com.virtualpairprogrammers.utilities.application
+
+import com.virtualpairprogrammers.utilities.toSentenceCase
+
+// myFunction is a lambda function which receives a String as parameter and returns another String
+fun applySomeFunctionToAString(inputString: String, myFunction: (String) -> String) : String {
+ return myFunction(inputString)
+}
+
+fun main() {
+    val result: String = applySomeFunctionToAString("hello") { x -> x[0].toUpperCase() + x.substring(1) }
+    val result1: String = applySomeFunctionToAString("hello") { it.toUpperCase() }
+    val result2: String = applySomeFunctionToAString("hello", ::toSentenceCase)
+    println(result)
+    println(result1)
+    println(result2)
+}
