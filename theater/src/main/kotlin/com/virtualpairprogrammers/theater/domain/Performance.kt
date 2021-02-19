10dd8106bce308172b503107db45b47958ed4a8a
felipe-basina <felipe.basina@gmail.com>
2021-02-19 09:17:56 -0300

    Adicionado entidade para reservas + mapeamento entre entidades.

--- theater/src/main/kotlin/com/virtualpairprogrammers/theater/domain/Performance.kt
+++ theater/src/main/kotlin/com/virtualpairprogrammers/theater/domain/Performance.kt
@@ -1,13 +1,15 @@
 package com.virtualpairprogrammers.theater.domain
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 data class Performance(@Id @GeneratedValue(strategy = GenerationType.AUTO)
                         val id: Long,
                         val title: String) {
+
+    @OneToMany(mappedBy = "performance")
+    lateinit var bookings: List<Booking>
+
     override fun toString(): String = "Performance: $id - $title"
+
 }
