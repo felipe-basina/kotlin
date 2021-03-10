e83b553753ae0898f5baeffa6f16d0232b800dd4
felipe-basina <felipe.basina@gmail.com>
2021-03-10 10:31:57 -0300

    Adicionado operacao para recuperar registros por id.

--- api-examples/src/main/kotlin/com/api/examples/api/FakeAPI.kt
+++ api-examples/src/main/kotlin/com/api/examples/api/FakeAPI.kt
@@ -3,9 +3,12 @@ package com.api.examples.api
 import com.api.examples.model.FakeModel
 import com.api.examples.repository.FakeRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.websocket.server.PathParam
 
 @RestController
 @RequestMapping(path = [ "/fake" ], produces = [ MediaType.APPLICATION_JSON_VALUE ])
@@ -17,6 +20,11 @@ class FakeAPI {
     @RequestMapping(path = [ "/all" ])
     fun all(): MutableIterable<FakeModel> {
         return this.fakeRepository.findAll()
+    }
+
+    @RequestMapping(path = [ "/{id}" ])
+    fun byId(@PathVariable(value = "id") id: Long): FakeModel? {
+        return this.fakeRepository.findByIdOrNull(id)
     }
 
 }
