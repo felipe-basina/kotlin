712b8f43018d2c73af06d363f7d7e241069d3afd
felipe-basina <felipe.basina@gmail.com>
2022-05-08 10:05:53 -0300

    Refatoração para enviar serviço de autorização como parâmetro.

--- kotlin-spring-lab/src/main/kotlin/spring/lab/aspect/AspectMainApplication.kt
+++ kotlin-spring-lab/src/main/kotlin/spring/lab/aspect/AspectMainApplication.kt
@@ -9,16 +9,17 @@ class KotlinSpringLabApplication
 
 fun main(args: Array<String>) {
 	val application = runApplication<KotlinSpringLabApplication>(*args)
+	val authorizationComponent = application.getBean(AuthorizationComponent::class.java)
 	val fakeService = application.getBean(FakeAService::class.java)
-	callService(fakeService, 1)
-	callService(fakeService, 0)
-	callService(fakeService, 5)
+	callService(fakeService, authorizationComponent, 1)
+	callService(fakeService, authorizationComponent,0)
+	callService(fakeService, authorizationComponent,5)
 	exitProcess(1)
 }
 
-fun callService(fakeAService: FakeAService, param: Int) {
+fun callService(fakeAService: FakeAService, authorizationComponent: AuthorizationComponent, param: Int) {
 	try {
-		fakeAService.fakeA(ParamObj(param))
+		fakeAService.fakeA(authorizationComponent, ParamObj(param))
 	} catch (ex : RuntimeException) {
 		println("Ex: ${ex.message}")
 	}
