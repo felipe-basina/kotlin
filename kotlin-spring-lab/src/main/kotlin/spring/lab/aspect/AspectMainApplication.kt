e687747b7b91546a02f3099d75eeafa5d472b47a
felipe-basina <felipe.basina@gmail.com>
2022-05-08 10:09:14 -0300

    Organizado identação de código.

--- kotlin-spring-lab/src/main/kotlin/spring/lab/aspect/AspectMainApplication.kt
+++ kotlin-spring-lab/src/main/kotlin/spring/lab/aspect/AspectMainApplication.kt
@@ -8,19 +8,19 @@ import kotlin.system.exitProcess
 class KotlinSpringLabApplication
 
 fun main(args: Array<String>) {
-	val application = runApplication<KotlinSpringLabApplication>(*args)
-	val authorizationComponent = application.getBean(AuthorizationComponent::class.java)
-	val fakeService = application.getBean(FakeAService::class.java)
-	callService(fakeService, authorizationComponent, 1)
-	callService(fakeService, authorizationComponent,0)
-	callService(fakeService, authorizationComponent,5)
-	exitProcess(1)
+    val application = runApplication<KotlinSpringLabApplication>(*args)
+    val authorizationComponent = application.getBean(AuthorizationComponent::class.java)
+    val fakeService = application.getBean(FakeAService::class.java)
+    callService(fakeService, authorizationComponent, 1)
+    callService(fakeService, authorizationComponent, 0)
+    callService(fakeService, authorizationComponent, 5)
+    exitProcess(1)
 }
 
 fun callService(fakeAService: FakeAService, authorizationComponent: AuthorizationComponent, param: Int) {
-	try {
-		fakeAService.fakeA(authorizationComponent, ParamObj(param))
-	} catch (ex : RuntimeException) {
-		println("Ex: ${ex.message}")
-	}
+    try {
+        fakeAService.fakeA(authorizationComponent, ParamObj(param))
+    } catch (ex: RuntimeException) {
+        println("Ex: ${ex.message}")
+    }
 }
