795920e31c8c0df105abc78507627c045a6874c5
felipe-basina <felipe.basina@gmail.com>
2022-06-23 18:22:35 -0300

    Adicionado estrutura inicial.

---
+++ poc-mvc/src/test/kotlin/poc/mvc/PocMvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package poc.mvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocMvcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
